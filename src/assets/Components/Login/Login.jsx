d452fa7909f358464319cbfacdc21e02888f0c58
Gustavo Rodrigues <gustarodrigues862@gmail.com>
2024-09-22 15:38:30 -0300

    Login

--- src/assets/Components/Login/Login.jsx
+++ src/assets/Components/Login/Login.jsx
@@ -1,25 +1,42 @@
 import { FaUser, FaLock } from 'react-icons/fa';
 
+import { useState } from 'react';
+
+import "./Login.css";
+
+// Componente de login com React Hooks
 const Login = () => {
   return (
-    <div>
-      <div className="Container">
-        <form>
-          <h1>Login</h1>
-          <div>
-            <input type="email" placeholder="E-mail" />
-            <FaUser className="icon"></FaUser>
-          </div>
-          <div>
-            <input type="password" placeholder="Senha" />
-            <FaLock className="icon"></FaLock>
-          </div>
-
-          <div className="recall-forget"></div>
-
-          <button>Entrar</button>
-        </form>
-      </div>
+    <div className="Container">
+      <form>
+        <h1>Login</h1>
+        <div>
+          <input type="email" placeholder="E-mail" />
+          <FaUser className="icon"></FaUser>
+        </div>
+        <div>
+          <input type="password" placeholder="Senha" />
+          <FaLock className="icon"></FaLock>
+        </div>
+        
+        {/* Elemento de lembar de mim */}
+        <div className="recall-forget"> 
+          <label>
+            <input type="checkbox" /> 
+            Lembre-me de mim
+          </label>
+          <a href="#">Esqueceu a senha?</a>
+        </div>
+          
+        <button>Entrar</button>
+        
+        <div className='signup-link'>
+          <p>
+            Não possui uma conta? <a href="#">Crie uma agora</a>
+          </p>
+      
+        </div>
+      </form>
     </div>
   );
 };
