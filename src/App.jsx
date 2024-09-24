bd10b0fca58d122df8eff8917d0d11f6c9e33bdf
Gustavo Rodrigues <gustarodrigues862@gmail.com>
2024-09-24 20:54:50 -0300

    Criação das rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
 import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './Components/Login/Login';
 import Cadastro from './Components/Cadastro/Cadastro';
+import Dashboard from './Components/Dashboard/Dashboard';
+import Alunos from './Components/Alunos/Alunos';
+import Professores from './Components/Professores/Professores';
+import Turmas from './Components/Turmas/Turmas';
+import Comunicados from './Components/Comunicados/Comunicados';
 
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Routes>
-          <Route path="/Login" element={<Login />} />
-          <Route path="/Cadastro" element={<Cadastro />} />
-          <Route path="/" element={<Login />} /> {/* Rota padrão */}
-        </Routes>
-      </div>
+      <Switch>
+        <Route path="/" exact component={Login} />
+        <Route path="/cadastro" component={Cadastro} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/alunos" component={Alunos} />
+        <Route path="/professores" component={Professores} />
+        <Route path="/turmas" component={Turmas} />
+        <Route path="/comunicados" component={Comunicados} />
+      </Switch>
     </Router>
   );
 }
