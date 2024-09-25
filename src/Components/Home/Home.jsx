001fec29d583bae3eef66ccd282c8632301206e3
Gustavo Rodrigues <152227687+GuusRodrigues@users.noreply.github.com>
2024-09-25 14:49:14 -0300

    teste

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,25 @@
 import { Link } from 'react-router-dom';
 import './Home.css';
+import React, { useState } from 'react';
 
 const Home = () => {
+
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
+
   return (
     <div className="home-container">
+      <nav className="navbar">
+        <div className="logo">
+          <img src="/vite.svg" alt="Logo" />
+        </div>
+
+
+        
+      </nav>
+
+
+
       <h1>Bem-vindo ao MedioTec</h1>
       <p id='tipo-login'>Escolha seu tipo de login:</p>
       <div className="login-options">
