1225fbe9de7d6dedcaaa56538e55ddd434e359c9
SenaaMatheus <matheussena2906@gmail.com>
2024-09-26 15:02:18 -0300

    atts

--- src/Components/Login/LoginCoord/LoginCoord.jsx
+++ src/Components/Login/LoginCoord/LoginCoord.jsx
@@ -1,49 +1,73 @@
-import { FaUser, FaLock } from 'react-icons/fa';
+import { FaUser, FaLock, FaTimes } from 'react-icons/fa'; // Importando FaTimes para o ícone de X
 import { useState } from 'react';
 import './LoginCoord.css';
 
-const LoginCoordenador = () => {
+const LoginProfessor = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Lógica para autenticar coordenadores
-    alert ("Login Coordenador: " + username + "-" + password);
+    // Lógica para autenticar
+    alert("Login Professor: " + username + " - " + password);
+  };
+
+  const handleGoHome = () => {
+    // Lógica para redirecionar para a página inicial
+    window.location.href = '/';  // Redireciona para a página inicial
   };
 
   return (
     <div className="Container">
-      <form onSubmit={handleSubmit}>
-        <h1 id="MedioTec">Login Coordenador</h1>
-        <div className='input-field'>
-          <input type="email" 
-          placeholder="Digite seu Email" 
-          onChange={(e) => setUsername(e.target.value)} 
-          />
-          <FaUser className="icon"></FaUser>
+      <div id="LoginBox">
+        {/* Ícone X no canto superior direito */}
+        <FaTimes className="closeIcon" onClick={handleGoHome} />
+
+        <div id="LogoCoord">
+          <img src="/img/logo.png" alt="Logo" />
         </div>
 
-        <div className='input-field'>
-          <input type="password" 
-          placeholder="Digite sua senha" 
-          onChange={(e) => setPassword(e.target.value)}
-          />
-          <FaLock className="icon"></FaLock>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <h1 id="MedioTec">LOGIN DO COORDENADOR</h1>
 
-        <div id="recal-forget"> 
-          <label id="label">
-          <input id="Checkbox" type="checkbox"/> 
-            <p id="Lembre">Lembre-me de mim</p>
-          </label>
-          <a href="#" id="Esqueceu">Esqueci minha senha</a>
-        </div>
-        
-        <button id="button">LOGIN</button>
-      </form>
+          <div className="input-field">
+            <input
+              type="email"
+              placeholder="Digite seu Email"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <FaUser className="icon" />
+          </div>
+
+          <div className="input-field">
+            <input
+              type="password"
+              placeholder="Digite sua senha"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <FaLock className="icon" />
+          </div>
+
+          <div id="recal-forget">
+            <label id="label">
+              <input id="Checkbox" type="checkbox" />
+              <p id="Lembre">Lembre-se de mim</p>
+            </label>
+            <a href="#" id="Esqueceu">
+              Esqueci minha senha
+            </a>
+          </div>
+
+          <button id="button">LOGIN</button>
+
+          {/* Botão para voltar à Home */}
+          <button type="button" id="homeButton" onClick={handleGoHome}>
+            Voltar
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
 
-export default LoginCoordenador;
+export default LoginProfessor;
--- src/Components/Login/LoginProf/LoginProf.jsx
+++ src/Components/Login/LoginProf/LoginProf.jsx
@@ -1,4 +1,4 @@
-import { FaUser, FaLock } from 'react-icons/fa';
+import { FaUser, FaLock, FaTimes } from 'react-icons/fa'; // Importando FaTimes para o ícone de X
 import { useState } from 'react';
 import './LoginProf.css';
 
@@ -9,40 +9,63 @@ const LoginProfessor = () => {
   const handleSubmit = (event) => {
     event.preventDefault();
     // Lógica para autenticar professores
-    alert ("Login Professor: " + username + "-" + password);
+    alert("Login Professor: " + username + " - " + password);
+  };
+
+  const handleGoHome = () => {
+    // Lógica para redirecionar para a página inicial
+    window.location.href = '/';  // Redireciona para a página inicial
   };
 
   return (
     <div className="Container">
-      <form onSubmit={handleSubmit}>
-        <h1 id="MedioTec">Login Professor</h1>
-        <div className='input-field'>
-          <input type="email" 
-          placeholder="Digite seu Email" 
-          onChange={(e) => setUsername(e.target.value)} 
-          />
-          <FaUser className="icon"></FaUser>
+      <div id="LoginBox">
+        {/* Ícone X no canto superior direito */}
+        <FaTimes className="closeIcon" onClick={handleGoHome} />
+
+        <div id="LogoProf">
+          <img src="/img/logo.png" alt="Logo" />
         </div>
 
-        <div className='input-field'>
-          <input type="password" 
-          placeholder="Digite sua senha" 
-          onChange={(e) => setPassword(e.target.value)}
-          />
-          <FaLock className="icon"></FaLock>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <h1 id="MedioTec">LOGIN DO PROFESSOR</h1>
 
-        {/* Elemento de lembar de mim */}
-        <div id="recal-forget"> 
-          <label id="label">
-          <input id="Checkbox" type="checkbox"/> 
-            <p id="Lembre">Lembre-me de mim</p>
-          </label>
-          <a href="#" id="Esqueceu">Esqueci minha senha</a>
-        </div>
-        
-        <button id="button">LOGIN</button>
-      </form>
+          <div className="input-field">
+            <input
+              type="email"
+              placeholder="Digite seu Email"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <FaUser className="icon" />
+          </div>
+
+          <div className="input-field">
+            <input
+              type="password"
+              placeholder="Digite sua senha"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <FaLock className="icon" />
+          </div>
+
+          <div id="recal-forget">
+            <label id="label">
+              <input id="Checkbox" type="checkbox" />
+              <p id="Lembre">Lembre-se de mim</p>
+            </label>
+            <a href="#" id="Esqueceu">
+              Esqueci minha senha
+            </a>
+          </div>
+
+          <button id="button">LOGIN</button>
+
+          {/* Botão para voltar à Home */}
+          <button type="button" id="homeButton" onClick={handleGoHome}>
+            Voltar
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
