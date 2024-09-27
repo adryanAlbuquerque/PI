bbaff947ce3a1e0c1ba09c813283df861e2349d4
Gustavo Rodrigues <gustarodrigues862@gmail.com>
2024-09-27 18:12:16 -0300

    tela de cadastro

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-//import Cadastro from './Components/Cadastro/Cadastro';
+import Cadastro from './Components/Cadastro/Cadastro';
 //import Dashboard from './Components/Dashboard/Dashboard';
 ///import Alunos from './Components/Alunos/Alunos';
 //import Professores from './Components/Professores/Professores';
@@ -11,6 +11,7 @@ import LoginAluno from './Components/Login/LoginAluno/LoginAluno';
 import LoginProf from './Components/Login/LoginProf/LoginProf';
 import LoginCoord from './Components/Login/LoginCoord/LoginCoord';
 
+
 function App() {
   return (
     <Router>
@@ -19,6 +20,7 @@ function App() {
         <Route path="/login/aluno" element={<LoginAluno />} />
         <Route path="/login/prof" element={<LoginProf />} />
         <Route path="/login/coord" element={<LoginCoord />} />
+        <Route path="/Cadastro/Cadastro" element={<Cadastro />} />
       </Routes>
     </Router>
   );
---
+++ src/Components/Gestão/Cadastro/Cadastro.jsx
@@ -0,0 +1,73 @@
+import { FaUser, FaLock, FaTimes, FaEnvelope  } from 'react-icons/fa'; // Importando FaTimes para o ícone de X
+import { useState } from 'react';
+import './Cadastro.css';
+
+const Cadastro = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Aqui você pode adicionar lógica para autenticar alunos
+    alert ("Enviando os dados:" + email + "-" + username + "-" + password);
+  };
+
+  const handleGoHome = () => {
+    // Lógica para redirecionar para a página inicial
+    window.location.href = '/';  // Redireciona para a página inicial
+  };
+
+  return (
+    <div className="Container">
+      <div id="LoginBox">
+        {/* Ícone X no canto superior direito */}
+        <FaTimes className="closeIcon" onClick={handleGoHome} />
+
+        <div id="LogoAluno">
+          <img src="/img/logo.png" alt="Logo" />
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <h1 id="MedioTec">CADASTRO</h1>
+
+          <div className="input-field">
+            <input
+              type="username"
+              placeholder="Nome Completo"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <FaUser className="icon" />
+          </div>
+          
+          <div className="input-field">
+            <input
+              type="email"
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <FaEnvelope  className="icon" />
+          </div>
+
+          <div className="input-field">
+            <input
+              type="password"
+              placeholder="Matrícula"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <FaLock className="icon" />
+          </div>
+
+          <button id="button">CADASTRAR</button>
+
+          {/* Botão para voltar à Home */}
+          <button type="button" id="homeButton" onClick={handleGoHome}>
+            Voltar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Cadastro;
