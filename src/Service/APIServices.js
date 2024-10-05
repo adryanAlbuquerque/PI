b0031b06274e0c6bd4861357cff7ebd355bd247b
Gustavo Rodrigues <gustarodrigues862@gmail.com>
2024-10-05 18:10:39 -0300

    correção

--- src/Service/APIServices.js
+++ src/Service/APIServices.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-///const API_URL = `${process.env.REACT_APP_API_URL}/usuarios`;
+//const API_URL = `${process.env.REACT_APP_API_URL}/usuarios`;
 const API_URL = 'http://localhost:8080/usuarios';
 
 
