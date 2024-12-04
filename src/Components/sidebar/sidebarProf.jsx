dd4bbdb098cb5898e44ebea08bef51129057aa40
Gustavo Rodrigues <gustarodrigues862@gmail.com>
2024-12-04 16:58:28 -0300

    ajuste css

--- src/Components/sidebar/sidebarProf.jsx
+++ src/Components/sidebar/sidebarProf.jsx
@@ -1,24 +1,22 @@
 import { Link } from 'react-router-dom';
-import './sidebarProf.css';
+import '../Themes/themesSidebar.css';
 import { useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa'; // Ícones para abrir/fechar
 
 
 const SidebarProf = () => {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false); // Sidebar fechada por padrão
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false); 
 
-  const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen); // Alterna a sidebar
+  const toggleSidebar = () => setIsSidebarOpen(!isSidebarOpen); 
 
   return (
-    <div className="sidebar-container-prof">
-      {/* Botão para abrir/fechar a sidebar */}
+    <div className="sidebar-container">
       <button className="sidebar-toggle-prof" onClick={toggleSidebar}>
       {isSidebarOpen ? <FaTimes /> : <FaBars />}
       </button>
 
-      {/* Sidebar */}
-      <div className={`sidebar-prof ${isSidebarOpen ? 'open' : 'closed'}`}>
-        <img id="MedioTecprof" src="/img/LogoProf.png" alt="Logo" />
+      <div className={`sidebar-list-prof ${isSidebarOpen ? 'open' : 'closed'}`}>
+        <img id="MedioTecSidebar" src="/img/LogoProf.png" alt="Logo" />
         <ul>
           <li><Link to="/HomeProf">Home</Link></li>
           <li><Link to="/DisciplinasProf">Disciplinas</Link></li>
@@ -26,7 +24,7 @@ const SidebarProf = () => {
           <li><Link to="/">Configurações</Link></li>
         </ul>
         <div>
-          <button type="button" className="Sairprof" onClick={() => window.location.href = '/'}>
+          <button type="button" className="Sairbutton-Prof" onClick={() => window.location.href = '/'}>
             Sair
           </button>
         </div>
